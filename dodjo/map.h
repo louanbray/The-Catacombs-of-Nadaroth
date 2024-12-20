f7e9cecd3a5b76218aa8173659f3df5a58ae33bd
louanbray <90617061+louanbray@users.noreply.github.com>
2024-12-20 15:18:32 +0100

    Update map.h

--- dodjo/map.h
+++ dodjo/map.h
@@ -12,5 +12,6 @@ map* create_map();
 chunk* getChunk(map* m, int x, int y);
 chunk* getChunkFrom(map* m, chunk* c1, Direction dir);
 void link(chunk* c1, chunk* c2, Direction dir);
+void destroy_chunk(map* m, chunk* ck);
 
 #endif
