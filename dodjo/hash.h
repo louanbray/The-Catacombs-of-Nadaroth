40ee990d80f9689486efe4a8c43b558a1e00ceaf
louanbray <90617061+louanbray@users.noreply.github.com>
2024-12-20 15:08:45 +0100

    Update hash.h

--- dodjo/hash.h
+++ dodjo/hash.h
@@ -6,14 +6,14 @@
 #include <stdlib.h>
 
 typedef struct hash_map_s hm;
-typedef void* chunk;
+typedef void* chunk_h;
 
 int hash(int h, int x, int y);
 hm* create();
 int len(hm* t);
 int size(hm* t);
 chunk* get(hm* t, int x, int y);
-void set(hm* t, int x, int y, chunk* e);
+void set(hm* t, int x, int y, chunk_h e);
 void purge(hm* t, int x, int y);
 void free_hm(hm* t);
 void print_hm(hm* t);
