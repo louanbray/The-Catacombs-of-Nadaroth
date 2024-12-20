78fa3e360d2e43fc814a978f30c92bebd1286b3c
louanbray <90617061+louanbray@users.noreply.github.com>
2024-12-20 15:12:35 +0100

    Update map.c

--- dodjo/map.c
+++ dodjo/map.c
@@ -1,2 +1,34 @@
-include "map.h";
+#include "hash.h";
+#include "map.h";
 
+enum Type;
+enum Direction {
+  STARGATE,
+  NORTH,
+  EAST,
+  SOUTH,
+  WEST
+};
+
+typedef void* elements;
+typedef struct chunk {
+  link link;
+  int x;
+  int y;
+  type Type;
+  elements elements;
+}
+
+typedef struct link {
+  chunk* stargate;
+  chunk* north;
+  chunk* east;
+  chunk* south;
+  chunk* west;
+}
+
+
+typedef struct map {
+  hm* hashmap;
+  chunk* spawn;
+} map;
