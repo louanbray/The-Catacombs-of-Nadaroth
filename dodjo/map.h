451669d097706ca67647ec894c0edc7f9f936524
louanbray <90617061+louanbray@users.noreply.github.com>
2024-12-20 14:42:36 +0100

    Update map.h

--- dodjo/map.h
+++ dodjo/map.h
@@ -2,4 +2,15 @@
 #ifndef MAP_H
 #define MAP_H
 
+typedef struct map map;
+typedef struct chunk chunk;
+typedef struct element element;
+typedef struct link link;
+enum Direction;
+
+map* create_map();
+chunk* getChunk(map* m, int x, int y);
+chunk* getChunkFrom(map* m, chunk* c1, Direction dir);
+void link(chunk* c1, chunk* c2, Direction dir);
+
 #endif
