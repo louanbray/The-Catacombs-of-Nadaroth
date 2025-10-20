030e819b838503371cdc4252c2a08800ea6c5ec1
Louan Bray <gogorille634@gmail.com>
2025-10-20 18:09:46 +0200

    Mise à jour des macros de journalisation pour inclure des horodatages et des informations de fichier

--- The Catacombs of Nadaroth/logger.h
+++ The Catacombs of Nadaroth/logger.h
@@ -7,9 +7,9 @@
 extern FILE* g_log_file;
 
 // Logging macros that write to the log file
-#define LOG_INFO(fmt, ...)  do { if (g_log_file) fprintf(g_log_file, "[INFO] " fmt "\n", ##__VA_ARGS__); fflush(g_log_file); } while(0)
-#define LOG_WARN(fmt, ...)  do { if (g_log_file) fprintf(g_log_file, "[WARN] " fmt "\n", ##__VA_ARGS__); fflush(g_log_file); } while(0)
-#define LOG_ERROR(fmt, ...) do { if (g_log_file) fprintf(g_log_file, "[ERROR] " fmt "\n", ##__VA_ARGS__); fflush(g_log_file); } while(0)
+#define LOG_INFO(fmt, ...)  do { if (g_log_file) {time_t now = time(NULL); struct tm* tm_info = localtime(&now); char timestamp[20]; strftime(timestamp, sizeof(timestamp), "%H:%M:%S", tm_info); fprintf(g_log_file, "[%s][INFO][%s:%d] " fmt "\n", timestamp, __FILE__, __LINE__, ##__VA_ARGS__); fflush(g_log_file);}} while(0)
+#define LOG_WARN(fmt, ...)  do { if (g_log_file) {time_t now = time(NULL); struct tm* tm_info = localtime(&now); char timestamp[20]; strftime(timestamp, sizeof(timestamp), "%H:%M:%S", tm_info); fprintf(g_log_file, "[%s][WARN][%s:%d] " fmt "\n", timestamp, __FILE__, __LINE__, ##__VA_ARGS__); fflush(g_log_file);}} while(0)
+#define LOG_ERROR(fmt, ...) do { if (g_log_file) {time_t now = time(NULL); struct tm* tm_info = localtime(&now); char timestamp[20]; strftime(timestamp, sizeof(timestamp), "%H:%M:%S", tm_info); fprintf(g_log_file, "[%s][ERROR][%s:%d] " fmt "\n", timestamp, __FILE__, __LINE__, ##__VA_ARGS__); fflush(g_log_file);}} while(0)
 
 void init_logger();
 void close_logger();
